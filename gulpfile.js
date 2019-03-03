8183489efcf7fe67a30576983bc6d454c933af2a
Dipen <dipensavji@gmail.com>
2019-03-03 23:02:17 +0530

    Gulp Basics
    Gulp-Basic functions 
    How ever Gulp-multi Task not Working 
    
    
    
    
    const gulp = require('gulp');
    const changed = require('gulp-changed');
    const imagemin = require('gulp-imagemin');
    const uglify = require('gulp-uglify');
    const sass = require('gulp-sass');
    const concat = require('gulp-concat'); 
    
    /*
    	--Basic Functons	
    	gulp.task  	// Define Tasks
    	gulp.src  	// Point Tofiles to use
    	gulp.dest 	// Points to folder to output
    	gulp.watch 	// Watch files and folders for Changes 
    */
    
    
    // Log Msg
    gulp.task('msg', ()=>{
        return console.log("Hello World");
    });
    
    
    
    // Copy Src to Dest All HTML files 
    gulp.task('copyHTML', ()=>{
    	gulp.src('src/**/*.html')
         .pipe(gulp.dest('dist/'));
    
       return console.log
        ("Html Files Copiedb successfully to dist Folder");
    
    });
    
    
    // Minify JavaScript 
    gulp.task('minjs', ()=>{
    	gulp.src('src/js/**/*.js')
    	  .pipe(uglify())
    	  .pipe(gulp.dest('dist/js'));
       return console.log
        ("Js files minified successfully");
    
    });
    
    
    // Image Minify 
    gulp.task('imagemin', ()=>{
    	var imgSrc = 'src/img/*.+(png|jpg|gif)';
       	var imgDst = 'dist/img';
       
    	gulp.src(imgSrc)
    	.pipe(changed(imgDst))
    	.pipe(imagemin())
    	.pipe(gulp.dest(imgDst));
    });
    
    
    
    // Sass Compile 
    gulp.task('sass', ()=>{
    	var sass_Src = 'src/sass/*.scss';
       	var sass_Dst = 'dist/css';
       
    	gulp.src(sass_Src)
    	.pipe(sass().on('error',sass.logError))
    	.pipe(gulp.dest(sass_Dst));
    });
    
    
    // JavaScript Minify + concat  
    gulp.task('script', ()=>{
    	gulp.src('src/js/**/*.js')
    	  .pipe(concat('main.js'))
    	  .pipe(uglify())
    	  .pipe(gulp.dest('dist/js'));
       return console.log
        ("Js files minified successfully");
    
    });
    
    
    
    // gulp.task('default',['script','imagemin'],function(){
    // 	return console.log("Default");
    // });
    
    // Default Gulp-Task
    // gulp.task('default',['script'],()=>{
    //    return console.log
    //     ("Default Task");
    // });
    
    
    
    // npm install
    // npm install -g gulp 
    // npm init
    // mkdir src dist
    
    // npm install --save-dev gulp
    // npm install --save-dev gulp-changed
    // npm install --save-dev gulp-imagemin
    // npm install --save-dev gulp-uglify
    // npm install --save-dev gulp-sass
    // npm install --save-dev gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,105 @@
 const gulp = require('gulp');
+const changed = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat'); 
 
 /*
-	--Basic Functons
-	
+	--Basic Functons	
 	gulp.task  	// Define Tasks
 	gulp.src  	// Point Tofiles to use
 	gulp.dest 	// Points to folder to output
 	gulp.watch 	// Watch files and folders for Changes 
-
 */
 
 
 // Log Msg
-gulp.task('msg', function() {
+gulp.task('msg', ()=>{
     return console.log("Hello World");
 });
 
-// Default Gulp-Task
-gulp.task('default', function() {
-   gulp.src('src/**/*.html')
+
+
+// Copy Src to Dest All HTML files 
+gulp.task('copyHTML', ()=>{
+	gulp.src('src/**/*.html')
      .pipe(gulp.dest('dist/'));
 
    return console.log
-   ("Html Files Copiedb successfully to Dist Folder");
-   
+    ("Html Files Copiedb successfully to dist Folder");
+
 });
+
+
+// Minify JavaScript 
+gulp.task('minjs', ()=>{
+	gulp.src('src/js/**/*.js')
+	  .pipe(uglify())
+	  .pipe(gulp.dest('dist/js'));
+   return console.log
+    ("Js files minified successfully");
+
+});
+
+
+// Image Minify 
+gulp.task('imagemin', ()=>{
+	var imgSrc = 'src/img/*.+(png|jpg|gif)';
+   	var imgDst = 'dist/img';
+   
+	gulp.src(imgSrc)
+	.pipe(changed(imgDst))
+	.pipe(imagemin())
+	.pipe(gulp.dest(imgDst));
+});
+
+
+
+// Sass Compile 
+gulp.task('sass', ()=>{
+	var sass_Src = 'src/sass/*.scss';
+   	var sass_Dst = 'dist/css';
+   
+	gulp.src(sass_Src)
+	.pipe(sass().on('error',sass.logError))
+	.pipe(gulp.dest(sass_Dst));
+});
+
+
+// JavaScript Minify + concat  
+gulp.task('script', ()=>{
+	gulp.src('src/js/**/*.js')
+	  .pipe(concat('main.js'))
+	  .pipe(uglify())
+	  .pipe(gulp.dest('dist/js'));
+   return console.log
+    ("Js files minified successfully");
+
+});
+
+
+
+// gulp.task('default',['script','imagemin'],function(){
+// 	return console.log("Default");
+// });
+
+// Default Gulp-Task
+// gulp.task('default',['script'],()=>{
+//    return console.log
+//     ("Default Task");
+// });
+
+
+
+// npm install
+// npm install -g gulp 
+// npm init
+// mkdir src dist
+
+// npm install --save-dev gulp
+// npm install --save-dev gulp-changed
+// npm install --save-dev gulp-imagemin
+// npm install --save-dev gulp-uglify
+// npm install --save-dev gulp-sass
+// npm install --save-dev gulp-concat

f2e37782d725880a79abe00bd611b5288540f6b4
DIpends <dipensavji@gmail.com>
2019-03-08 23:08:51 +0530

    Gulp Setup Done BrowserReload, Html.Css,js Minify, concat  Done
    //  npm install --save-dev gulp -g
    //  npm install --save-dev gulp-sass
    //  npm install --save-dev gulp-postcss autoprefixer cssnano gulp-sourcemaps
    //  npm install --save-dev browser-sync
    // 	npm install --save-dev gulp-htmlmin
    //  ---OR---
    // 	All on one File
    // 	npm i --save-dev gulp gulp-sass gulp-postcss autoprefixer cssnano gulp-sourcemaps browser-sync gulp-htmlmin
    
    // gulpfile.js
    var gulp = require("gulp"),
    	htmlmin = require('gulp-htmlmin'),
        sass = require("gulp-sass"),
        postcss = require("gulp-postcss"),
        cssnano = require("cssnano"),
        autoprefixer = require("autoprefixer"),
        sourcemaps = require("gulp-sourcemaps"),
        //for_JavaScript
    	concat = require('gulp-concat'),
    	uglify = require('gulp-uglify');
    
    var browserSync = require("browser-sync").create();
    
    var src="src/", dest="dest/";
    
    var paths = {
    	html:{src: src+"**/*.html", dest:dest },
    	styles:{ src: src+"sass/**/*.scss", dest:dest+"assets/style/" },
    	scripts:{ src: src+"script/**/*.js", dest:dest+"assets/script/" }
    };
    
    // Html minify
    function html(){
    	return gulp.src(paths.html.src)
    		.pipe(htmlmin({collapseWhitespace: true}))
    		.pipe(gulp.dest(paths.html.dest));
    }
    exports.html = html;
    
    
    // Define tasks after requiring dependencies
    function style() {
        return (
            gulp
                .src(paths.styles.src)
                // Initialize sourcemaps before compilation starts
                .pipe(sourcemaps.init())
                .pipe(sass())
                .on("error", sass.logError)
                // Use postcss with autoprefixer and compress the compiled file using cssnano
                .pipe(postcss([autoprefixer(), cssnano()]))
                // Now add/write the sourcemaps
                .pipe(sourcemaps.write())
                .pipe(gulp.dest(paths.styles.dest))
        );
    }
    // Expose the task by exporting it
    // This allows you to run it from the commandline using
    // $ gulp style
    exports.style = style;
    // Cmd Command
    // gulp style
    
    
    
    function script() {
      return gulp.src(paths.scripts.src)
    	.pipe(uglify())
    	.pipe(concat('main.min.js'))
    	.pipe(gulp.dest(paths.scripts.dest))
    	.pipe(browserSync.stream())
    }
    var js = gulp.series(script);
    exports.js = js;
    
    
    
    // A simple task to reload the page
    function reload() {
        browserSync.reload();
    }
    
    
    // Add browsersync initialization at the start of the watch task
    function watch() {
        browserSync.init({
            // You can tell browserSync to use this directory and serve it as a mini-server
            server: {
                baseDir: "./"
            }
            // If you are already serving your website locally using something like apache
            // You can use the proxy setting to proxy that instead
            // proxy: "yourlocal.dev"
        });
    
        // gulp.watch takes in the location of the files to watch for changes
        // and the name of the function we want to run on change
        gulp.watch(src+"**/*",gulp.series(style,html,script));
    
        // We should tell gulp which files to watch to trigger the reload
        // This can be html or whatever you're using to develop your website
        // Note -- you can obviously add the path to the Paths object
        gulp.watch(src+"**/*",reload);
    }
    // Don't forget to expose the task!
    exports.watch = watch;

--- gulpfile.js
+++ gulpfile.js
@@ -40,23 +40,6 @@
 //   cb();
 // }
 
-// const { watch } = require('gulp');
-// const watcher = watch(['src/**/*.html']);
-
-// watcher.on('change', function(path, stats) {
-//   console.log(`File ${path} was changed`);
-// });
-
-// watcher.on('add', function(path, stats) {
-//   console.log(`File ${path} was added`);
-// });
-
-// watcher.on('unlink', function(path, stats) {
-//   console.log(`File ${path} was removed`);
-// });
-
-// watcher.close();
-
 // exports.build = series(clean, parallel( cssTranspile, series(jsTranspile, jsBundle) ),
 // 							  parallel( cssMinify, jsMinify), publish
 // );
@@ -68,34 +51,57 @@
 
 //  npm install --save-dev gulp -g
 //  npm install --save-dev gulp-sass
-
+//  npm install --save-dev gulp-postcss autoprefixer cssnano gulp-sourcemaps
+//  npm install --save-dev browser-sync
+// 	npm install --save-dev gulp-htmlmin
+//  ---OR---
+// 	All on one File
+// 	npm i --save-dev gulp gulp-sass gulp-postcss autoprefixer cssnano gulp-sourcemaps browser-sync gulp-htmlmin
 
 // gulpfile.js
+var gulp = require("gulp"),
+	htmlmin = require('gulp-htmlmin'),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    cssnano = require("cssnano"),
+    autoprefixer = require("autoprefixer"),
+    sourcemaps = require("gulp-sourcemaps"),
+    //for_JavaScript
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+
+var src="src/", dest="dest/";
+
 var paths = {
-	html:{src:"src/**/*.html", dest:"dest/" },
-	styles:{ src:"src/sass/**/*.scss", dest:"dest/assets/style/" },
-	scripts:{ src:"src/script/**/*.js", dest:"dest/assets/script/" }
+	html:{src: src+"**/*.html", dest:dest },
+	styles:{ src: src+"sass/**/*.scss", dest:dest+"assets/style/" },
+	scripts:{ src: src+"script/**/*.js", dest:dest+"assets/script/" }
 };
 
+// Html minify
+function html(){
+	return gulp.src(paths.html.src)
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest(paths.html.dest));
+}
+exports.html = html;
 
 
 // Define tasks after requiring dependencies
 function style() {
-    // Where should gulp look for the sass files?
-    // My .sass files are stored in the styles folder
-    // (If you want to use scss files, simply look for *.scss files instead)
     return (
         gulp
             .src(paths.styles.src)
- 
-            // Use sass with the files found, and log any errors
+            // Initialize sourcemaps before compilation starts
+            .pipe(sourcemaps.init())
             .pipe(sass())
             .on("error", sass.logError)
- 
-            // What is the destination for the compiled file?
+            // Use postcss with autoprefixer and compress the compiled file using cssnano
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            // Now add/write the sourcemaps
+            .pipe(sourcemaps.write())
             .pipe(gulp.dest(paths.styles.dest))
     );
 }
@@ -103,15 +109,50 @@ function style() {
 // This allows you to run it from the commandline using
 // $ gulp style
 exports.style = style;
-// CLI Command
+// Cmd Command
 // gulp style
 
-// to Set Watch()
-function watch(){
+
+
+function script() {
+  return gulp.src(paths.scripts.src)
+	.pipe(uglify())
+	.pipe(concat('main.min.js'))
+	.pipe(gulp.dest(paths.scripts.dest))
+	.pipe(browserSync.stream())
+}
+var js = gulp.series(script);
+exports.js = js;
+
+
+
+// A simple task to reload the page
+function reload() {
+    browserSync.reload();
+}
+
+
+// Add browsersync initialization at the start of the watch task
+function watch() {
+    browserSync.init({
+        // You can tell browserSync to use this directory and serve it as a mini-server
+        server: {
+            baseDir: "./"
+        }
+        // If you are already serving your website locally using something like apache
+        // You can use the proxy setting to proxy that instead
+        // proxy: "yourlocal.dev"
+    });
+
     // gulp.watch takes in the location of the files to watch for changes
     // and the name of the function we want to run on change
-    gulp.watch(paths.styles.src, style)
+    gulp.watch(src+"**/*",gulp.series(style,html,script));
+
+    // We should tell gulp which files to watch to trigger the reload
+    // This can be html or whatever you're using to develop your website
+    // Note -- you can obviously add the path to the Paths object
+    gulp.watch(src+"**/*",reload);
 }
-   
 // Don't forget to expose the task!
-exports.watch = watch
+exports.watch = watch;
+   
